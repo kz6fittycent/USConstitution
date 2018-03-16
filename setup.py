805ed8d112e791e3ddb87cf0155cb9a6daeb3797
kz6fittycent <jimmy.tigert@gmail.com>
2018-03-16 01:15:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="usconstitution",
-    version="1.0",
+    version="1.1",
     description="Read the US Constitution right from your terminal",
     long_description=open("README.md").read(),
     license="MIT",
